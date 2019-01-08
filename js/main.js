33556f5b3706b282eb8d89dfb869be21afe14672
souzabruna <brunaelisangelasouza@gmail.com>
2019-01-08 13:02:59 -0200

    commit

--- js/main.js
+++ js/main.js
@@ -20,19 +20,19 @@ function dadosItem1(hora,hoje,ontem,media) {
       data: {
         labels: dadoshora,
         datasets: [
-          { 
+          {
             data: dadoshoje,
             label: "Hoje",
             borderColor: "#7540cc",
             fill: false
           },
-          { 
+          {
             data: dadosontem,
             label: "Ontem",
             borderColor: "#25a1bd",
             fill: false
           },
-          { 
+          {
             data: dadosmedia,
             label: "Média",
             borderColor: "#444444",
@@ -46,15 +46,35 @@ function dadosItem1(hora,hoje,ontem,media) {
 
 }
 
-function dadosCampanha(status){
+function dadosCampanha(status, date, hour, inbound, outbound, difference){
     var status= status.split('%');
+    var date = date;
+    var difference= difference;
+    var differenceNumber = difference.split('%');
+    differenceNumber = parseFloat(differenceNumber[0]);
+    var outbound = outbound;
+    var hour = hour;
+    var inbound = inbound;
+    $('#campanha-inbound').text(inbound);
+    $('#campanha-outbound').text(outbound);
+    $('#campanha-date').text(date);
+    $('#campanha-hour').text(hour);
+
+    if(differenceNumber > 0) {
+      $("#sub-campanha").addClass("green-background");
+    }else if (differenceNumber < 0) {
+      $("#sub-campanha").addClass("red-background");
+    } else if (differenceNumber == 0){
+      $('#sub-campanha').addClass("yellow-background");
+    }
+
     var ct = document.getElementById("campanha").getContext('2d');
     var myChart = new Chart(ct, {
       type: 'doughnut',
       data: {
         labels:["Campaigns"],
         datasets: [
-          { 
+          {
             data: status,
             label: "Campaigns",
             borderColor: "#242424",
@@ -74,9 +94,30 @@ function dadosCampanha(status){
     });
 }
 
-function dadosNotification(notfications){
-    var notfications= notfications.split('%'); 
+function dadosNotification(notfications, date, hour, inbound, outbound, difference){
+    var notfications= notfications.split('%');
     var resto = 100 - notfications[0];
+    var date = date;
+    var hour = hour;
+    var outbound = outbound;
+    var inbound = inbound;
+    $('#notification-inbound').text(inbound);
+    $('#notification-outbound').text(outbound);
+    $('#notification-date').text(date);
+    $('#notification-hour').text(hour);
+
+    var difference= difference;
+    var differenceNumber = difference.split('%');
+    differenceNumber = parseFloat(differenceNumber[0]);
+
+    if(differenceNumber > 0) {
+      $("#sub-not").addClass("green-background");
+    }else if (differenceNumber < 0) {
+      $("#sub-not").addClass("red-background");
+    } else if (differenceNumber == 0){
+      $('#sub-not').addClass("yellow-background");
+    }
+
 
     var nots = document.getElementById("notification").getContext('2d');
     var myChart = new Chart(nots, {
@@ -84,7 +125,7 @@ function dadosNotification(notfications){
       data: {
         labels:["Notfications"],
         datasets: [
-          { 
+          {
             data: notfications,
             label: "Campaigns",
             borderColor: "#242424",
@@ -97,11 +138,29 @@ function dadosNotification(notfications){
 }
 
 
-function dadosTransaction(transactions){
+function dadosTransaction(transactions, date, hour, inbound, outbound, difference){
     var transactions= transactions.split('%');
-    
-    var resto = 100 - transactions[0];
-    console.log(resto);
+    var date = date;
+    var hour = hour;
+    var outbound = outbound;
+    var inbound = inbound;
+    var difference= difference;
+    var differenceNumber = difference.split('%');
+    differenceNumber = parseFloat(differenceNumber[0]);
+
+    $('#transaction-inbound').text(inbound);
+    $('#transaction-outbound').text(outbound);
+    $('#transaction-date').text(date);
+    $('#transaction-hour').text(hour);
+
+    if(differenceNumber > 0) {
+      $("#sub-transaction").addClass("green-background");
+    }else if (differenceNumber < 0) {
+      $("#sub-transaction").addClass("red-background");
+    } else if (differenceNumber == 0){
+      $('#sub-transaction').addClass("yellow-background");
+    }
+
 
     var trans = document.getElementById("transactions").getContext('2d');
     var myChart = new Chart(trans, {
@@ -109,9 +168,9 @@ function dadosTransaction(transactions){
       data: {
         labels:["Transactions"],
         datasets: [
-          { 
+          {
             data: transactions,
-            label: "Campaigns",
+            label: "Transactions",
             borderColor: "#242424",
             backgroundColor:"#3bb24c",
             cutoutPercentage:"90",
@@ -140,7 +199,7 @@ $(document).ready(function(){
         dataType: "json",
         url: "http://pmweb.agencia.pmweb.com.br/teste-frontend/api/intranet/campaigns.json"
     }).done(function(data){
-        dadosCampanha(data.healthstatus)
+        dadosCampanha(data.healthstatus, data.date, data.time, data.inbound, data.outbound, data.difference);
     });
 });
 
@@ -150,7 +209,7 @@ $(document).ready(function(){
         dataType: "json",
         url: "http://pmweb.agencia.pmweb.com.br/teste-frontend/api/intranet/notification.json"
     }).done(function(data){
-        dadosNotification(data.healthstatus)
+        dadosNotification(data.healthstatus, data.date, data.time, data.inbound, data.outbound, data.difference);
     });
 });
 
@@ -160,8 +219,7 @@ $(document).ready(function(){
         dataType: "json",
         url: "http://pmweb.agencia.pmweb.com.br/teste-frontend/api/intranet/transaction.json"
     }).done(function(data){
-       dadosTransaction(data.healthstatus)
-
+       dadosTransaction(data.healthstatus,  data.date, data.time, data.inbound, data.outbound, data.difference);
     });
 });
 
@@ -171,9 +229,4 @@ $('#open-modal').click(function(){
 
 $('.modal-close').click(function(){
     $('.modal').toggle(".modal-open");
-});
-
-
-$(function(){
-    $("#calendario").datepicker({dateFormat: 'dd/mm/yyyy'});
 });
