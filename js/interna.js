0ebbb94b3c9fce6107d26bb28cbfe09354de6c9c
souzabruna <brunaelisangelasouza@gmail.com>
2019-01-09 16:58:26 -0200

    ajustar fonts

--- js/interna.js
+++ js/interna.js
@@ -25,11 +25,11 @@ $(document).ready(function(){
              $('.kind-div:last-child .numberCampaing:last-child').after('<div class="statusCampaing">'+  data.campaigns[i].healthstatus +'</div>');
              $('.kind-div:last-child .statusCampaing:last-child').after('<div class="createdCampaing">'+  data.campaigns[i].created +'</div>');
              $('.kind-div:last-child .createdCampaing:last-child').after('<div class="periodCampaing">'+  data.campaigns[i].period +'</div>');
-             $('.kind-div:last-child .periodCampaing:last-child').after('<div class="content-campains"><div class="texts-campain">Campanha</div><div class="texts-campain">Objetivo</div><div class="texts-campain">Impressões</div><div class="texts-campain">Cliques</div><div class="texts-campain">CTR</div><div class="texts-campain">CPM(R$)</div><div class="texts-campain">CPC(R$) </div><div class="texts-campain">Custos(R$) </div></div>');
+             $('.kind-div:last-child .periodCampaing:last-child').after('<div class="content-campains"><div class="texts-campain">Campanha</div><div class="texts-campain">Objetivo</div><div class="texts-campain">Impressões</div><div class="texts-campain texts-campain-clic">Cliques</div><div class="texts-campain texts-campain-clic">CTR</div><div class="texts-campain texts-campain-clic">CPM(R$)</div><div class="texts-campain texts-campain-clic">CPC(R$) </div><div class="texts-campain">Custos(R$) </div></div>');
              var k;
 
              for (k=0; k < j; k++){
-               $('.kind-div:last-child .content-campains:last-child').after('<div class="content-campains"><div class="content-campains-item">' + data.campaigns[i].campaign[k].name + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].goal + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].impressions +  '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].clicks + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].ctr + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].cpm + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].cpc + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].custo + '</div></div>');
+               $('.kind-div:last-child .content-campains:last-child').after('<div class="content-campains"><div class="content-campains-item">' + data.campaigns[i].campaign[k].name + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].goal + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].impressions +  '</div><div class="content-campains-item content-campains-item-clic">' + data.campaigns[i].campaign[k].clicks + '</div><div class="content-campains-item content-campains-item-clic">' + data.campaigns[i].campaign[k].ctr + '</div><div class="content-campains-item content-campains-item-clic">' + data.campaigns[i].campaign[k].cpm + '</div><div class="content-campains-item content-campains-item-clic">' + data.campaigns[i].campaign[k].cpc + '</div><div class="content-campains-item">' + data.campaigns[i].campaign[k].custo + '</div></div>');
              }
 
            i++;
@@ -39,4 +39,8 @@ $(document).ready(function(){
           $(this).find('.content-campains').toggleClass('flex-display');
         });
     });
+});
+
+$('.close-page').click(function() {
+  window.location.assign('index.html');
 });
