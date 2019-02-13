2db4141df7ca96dd0e91de24c850f41002707015
souzabruna <brunaelisangelasouza@gmail.com>
2019-02-13 20:08:55 -0200

    reestrutura arquivos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 app.get('/', function (req, res) {
-   res.sendfile('index.html');
+   res.sendfile('src/views/index.html');
 })
 
 app.use(express.static('src'))
